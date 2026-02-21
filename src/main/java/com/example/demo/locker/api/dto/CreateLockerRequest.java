876ad98515bc1c84fddacb014419d1c17d570f44
Vladyslav Dorosh <vladyslav.dorosh@student.tuke.sk>
2026-02-21 10:44:21 +0100

    add notnull constraint

--- src/main/java/com/example/demo/locker/api/dto/CreateLockerRequest.java
+++ src/main/java/com/example/demo/locker/api/dto/CreateLockerRequest.java
@@ -1,8 +1,9 @@
 package com.example.demo.locker.api.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CreateLockerRequest(
-        @Positive
+        @NotNull @Positive
         Integer number
 ) {}
